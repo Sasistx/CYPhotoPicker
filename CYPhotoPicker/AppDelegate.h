2a216340dda8315dec2c064c6167104f017e3e95
gaotianxiang <gaotianxiang@chunyu.me>
2016-01-07 15:40:37 +0800

    Initial Commit

---
+++ CYPhotoPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYPhotoPicker
+//
+//  Created by 高天翔 on 16/1/7.
+//  Copyright © 2016年 CYGTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CYPhotoPicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CYPhotoPicker
+//
+//  Created by 高天翔 on 16/1/7.
+//  Copyright © 2016年 CYGTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
